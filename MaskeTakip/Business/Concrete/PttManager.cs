5adb103ad7d2c39fff457c205e085bb7672349aa
Kerem Gençer <keremgencer193@gmail.com>
2024-02-07 18:54:44 +0300

    Ödev tamamlandı

---
+++ MaskeTakip/Business/Concrete/PttManager.cs
@@ -0,0 +1,30 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        private IAplicantService _aplicantService;
+        public PttManager(IAplicantService aplicantService)
+        {
+            _aplicantService = aplicantService;
+        }
+        public void GiveMask(Person person)
+        {
+            if (_aplicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " " + person.LastName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " " + person.LastName + " için maske VERİLEMEDİ");
+            }
+        }
+    }
+}
---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,4 @@
+﻿using Business.Concrete;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(new Entities.Concrete.Person() { FirstName = "isim", LastName = "soyisim", DateOfBirthYear = 2024, NationalIdentity = 12345678905 });
